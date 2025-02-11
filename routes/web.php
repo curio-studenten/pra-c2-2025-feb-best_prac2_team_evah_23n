73ec276c7bbbbd2b1addea6a7cf956c88b350c54
levistinkt <sdestinkerd@gmail.com>
2025-02-11 15:24:54 +0100

    breeze install
    installatie van breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,51 +9,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-/*
-2017-10-30 setup for urls
-Home:			/
-Brand:			/52/AEG/
-Type:			/52/AEG/53/Superdeluxe/
-Manual:			/52/AEG/53/Superdeluxe/8023/manual/
-				/52/AEG/456/Testhandle/8023/manual/
-
-If we want to add product categories later:
-Productcat:		/category/12/Computers/
-*/
-
-use App\Models\Brand;
-use App\Http\Controllers\RedirectController;
-use App\Http\Controllers\BrandController;
-use App\Http\Controllers\TypeController;
-use App\Http\Controllers\ManualController;
-use App\Http\Controllers\ProductCategoryController;
-use App\Http\Controllers\SitemapController;
-use App\Http\Controllers\LocaleController;
-
-// Homepage
 Route::get('/', function () {
-    $brands = Brand::all()->sortBy('name');
-    return view('pages.homepage', compact('brands'));
-})->name('home');
+    return view('welcome');
+});
 
-Route::get('/manual/{language}/{brand_slug}/', [RedirectController::class, 'brand']);
-Route::get('/manual/{language}/{brand_slug}/brand.html', [RedirectController::class, 'brand']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/datafeeds/{brand_slug}.xml', [RedirectController::class, 'datafeed']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Locale routes
-Route::get('/language/{language_slug}/', [LocaleController::class, 'changeLocale']);
-
-// List of manuals for a brand
-Route::get('/{brand_id}/{brand_slug}/', [BrandController::class, 'show']);
-
-// Detail page for a manual
-Route::get('/{brand_id}/{brand_slug}/{manual_id}/', [ManualController::class, 'show']);
-
-// Generate sitemaps
-Route::get('/generateSitemap/', [SitemapController::class, 'generate']);
+require __DIR__.'/auth.php';
