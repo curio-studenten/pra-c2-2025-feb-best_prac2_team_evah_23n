65de11dead73a684814d613b54248c399ed35a6b
levistinkt <sdestinkerd@gmail.com>
2025-02-21 13:21:08 +0100

    ticket 9
    LET OP : sql is ook aangepast. als je pagina niet laad doe dan een rollback migration

--- resources/views/pages/manual_list.blade.php
+++ resources/views/pages/manual_list.blade.php
@@ -8,22 +8,23 @@
         <li><a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/" alt="Manuals for '{{$brand->name}}'" title="Manuals for '{{$brand->name}}'">{{ $brand->name }}</a></li>
     </x-slot:breadcrumb>
 
-
     <h1>{{ $brand->name }}</h1>
 
     <p>{{ __('introduction_texts.type_list', ['brand'=>$brand->name]) }}</p>
 
+    @foreach ($manuals as $manual)
 
-        @foreach ($manuals as $manual)
+        {{-- Increment the view count.  Important: Do this *before* displaying the manual link --}}
+        <?php $manual->views++; $manual->save(); ?>  {{-- Or $manual->views++; $manual->save(); if you prefer --}}
 
-            @if ($manual->locally_available)
-                <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
-                ({{$manual->filesize_human_readable}})
-            @else
-                <a href="{{ $manual->url }}" target="new" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
-            @endif
+        @if ($manual->locally_available)
+            <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
+            ({{$manual->filesize_human_readable}})
+        @else
+            <a href="{{ $manual->url }}" target="new" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
+        @endif
 
-            <br />
-        @endforeach
-
+        <br />
+    @endforeach
+    <p>Weergaven: {{ $manual->views }}</p>
 </x-layouts.app>
