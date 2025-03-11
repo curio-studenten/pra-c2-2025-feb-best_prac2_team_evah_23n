8e5099b3dea645ac28af8a3e1cbf6a1ba1e654b5
adilelka06 <adilelka06@hotmail.com>
2025-03-11 14:39:17 +0100

    Revert "ticket 6"
    This reverts commit af9efc17c58d21257a47092d410b6f086ab15773.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,36 +1,17 @@
 
 <footer>
     <div class="container">
-        <div class="contact">
-            {{-- contact gegevens --}}
-            <h2>Contact</h2>
-            <p>levi@4shop.com</p>
-            <p>06 12345678</p>
-
-        </div>
-        <div class="about">
-            <h2>over ons</h2>
-            <p>Welkom bij 4S, jouw betrouwbare bron voor handleidingen en gebruikersgidsen. Wij begrijpen hoe belangrijk het is om snel en eenvoudig toegang te hebben tot de juiste informatie wanneer je een product gebruikt. Daarom hebben we een uitgebreide database met handleidingen en gebruikersgidsen samengesteld. Zoek eenvoudig op productnaam en vind de handleiding die je nodig hebt.</p>
-        </div>
-
-        <div class="socials">
-            {{-- social media links --}}
-            <h2>Volg ons</h2>
-            <a href="https://www.facebook.com/">facebook</a>
-            <a href="https://www.instagram.com/">instagram</a>
-            <a href="https://www.twitter.com/">twitter</a>
-        </div>
-    </div>
 	    © {{ __('misc.copyright') }}
+    </div>
 </footer>
 
 
 <!-- analytics code -->
 <script type="text/javascript">
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-30506707-1']);
-_gaq.push(['_trackPageview']);
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-30506707-1']);
+  _gaq.push(['_trackPageview']);
 
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
