93e98c6f881b006833aefcf4558d8646aa53e17e
KevinRudyHsieh <117444524+KevinRudyHsieh@users.noreply.github.com>
2025-03-11 01:07:27 +0100

    ticket 5??
    i gave up maar ik heb het in een soort grid gezet

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -24,38 +24,21 @@
 
     <div class="container">
         <!-- Example row of columns -->
-        <div class="row">
-
-            @foreach($brands->chunk($chunk_size) as $chunk)
-                <div class="col-md-4">
-
-                    <ul>
+        <div class="brands-container">
+            @foreach($brands->groupBy(fn($brand) => strtoupper(substr($brand->name, 0, 1))) as $letter => $chunk)
+                <div class="brand-group">
+                    <h2>{{ $letter }}</h2>
+                    <div class="brands-grid">
                         @foreach($chunk as $brand)
-
-                            <?php
-                            $current_first_letter = strtoupper(substr($brand->name, 0, 1));
-
-                            if (!isset($header_first_letter) || (isset($header_first_letter) && $current_first_letter != $header_first_letter)) {
-                                echo '</ul>
-						<h2>' . $current_first_letter . '</h2>
-						<ul>';
-                            }
-                            $header_first_letter = $current_first_letter
-                            ?>
-
-                            <li>
-                                <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/">{{ $brand->name }}</a>
-                            </li>
+                            <div class="brand-item">
+                                <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/">
+                                    {{ $brand->name }}
+                                </a>
+                            </div>
                         @endforeach
-                    </ul>
-
+                    </div>
                 </div>
-                <?php
-                unset($header_first_letter);
-                ?>
             @endforeach
-
         </div>
-
     </div>
 </x-layouts.app>
